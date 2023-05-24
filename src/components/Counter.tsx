7c2beaef2b15fd598c2139ec98abd319df0b6140
paresiqbal <thecrowdel@gmail.com>
2023-05-24 13:32:29 +0700

    Change inline function into self function

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,13 +1,29 @@
-import { useState } from "react";
+import { useCallback, useEffect, useState } from "react";
+
+interface User {
+  id: number;
+  username: string;
+}
 
 export default function Counter() {
   const [count, setCount] = useState<number>(1);
+  const [users, setUsers] = useState<User[] | null>(null);
+
+  useEffect(() => {
+    console.log("Mounting");
+    console.log("User", users);
+
+    return () => console.log("Unmounting");
+  }, [users]);
+
+  const addOne = useCallback(() => setCount((prev) => prev + 1), []);
+  const decreaseOne = useCallback(() => setCount((prev) => prev - 1), []);
 
   return (
     <div>
       <h1>Count is {count}</h1>
-      <button onClick={() => setCount((prev) => prev + 1)}>+</button>
-      <button onClick={() => setCount((prev) => prev - 1)}>-</button>
+      <button onClick={addOne}>+</button>
+      <button onClick={decreaseOne}>-</button>
     </div>
   );
 }
