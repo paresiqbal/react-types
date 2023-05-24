5e94656061f2123289821750ccefd9c053c5d840
paresiqbal <thecrowdel@gmail.com>
2023-05-24 13:42:12 +0700

    using useMemo

--- src/components/Counter.tsx
+++ src/components/Counter.tsx
@@ -1,9 +1,18 @@
-import { useCallback, useEffect, useState } from "react";
+import { useCallback, useEffect, useMemo, useState } from "react";
 
 interface User {
   id: number;
   username: string;
 }
+
+type fibFunc = (n: number) => number;
+
+const fib: fibFunc = (n) => {
+  if (n < 2) return n;
+  return fib(n - 1) + fib(n - 2);
+};
+
+const myNumb: number = 20;
 
 export default function Counter() {
   const [count, setCount] = useState<number>(1);
@@ -19,11 +28,14 @@ export default function Counter() {
   const addOne = useCallback(() => setCount((prev) => prev + 1), []);
   const decreaseOne = useCallback(() => setCount((prev) => prev - 1), []);
 
+  const result = useMemo<number>(() => fib(myNumb), [myNumb]);
+
   return (
     <div>
       <h1>Count is {count}</h1>
       <button onClick={addOne}>+</button>
       <button onClick={decreaseOne}>-</button>
+      <h2>{result}</h2>
     </div>
   );
 }
